273cd9b0d1242e7388e09f08977623d746016b81
gxs <gouxiaosong1001@gmail.com>
2025-10-31 23:58:28 +0800

    init

--- packages/api-client/vitest.config.ts
+++ packages/api-client/vitest.config.ts
@@ -15,7 +15,12 @@ export default defineConfig({
         "**/*/playground.ts",
       ],
       thresholds: {
-        "100": true,
+          global: {
+              lines: 80,
+              functions: 80,
+              branches: 80,
+              statements: 80,
+          },
       },
     },
   },
